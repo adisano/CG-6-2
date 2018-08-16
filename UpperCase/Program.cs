f080f1d7c361620bc65adc4f5c712771d3563047
Adriana Disano <adrianadisano@gmail.com>
2018-08-16 18:18:35 -0500

    -Updated code comments.

--- UpperCase/Program.cs
+++ UpperCase/Program.cs
@@ -6,8 +6,12 @@ namespace UpperCase
     {
         static void Main(string[] args)
         {
+            //prompt user for input
             Console.WriteLine("Enter a word:");
 
+            //write user input in ALLCAPS
+            //(I know this isn't very readable...)
+            //(But I am kind of proud of myself for making this program so tiny. :P)
             Console.WriteLine(Console.ReadLine().ToUpper());
 
             //keep program open
